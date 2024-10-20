9b8e5c341b22ed9336e92a8b3618cae4886952f5
Lychtael <jitabilhaq12@gmail.com>
2024-10-20 10:25:24 +0700

    Video 17 - Namespace

---
+++ namespace/App/Produk/CetakInfoProduk.php
@@ -0,0 +1,15 @@
+<?php namespace App\Produk;
+
+class CetakInfoProduk {
+    public $daftarProduk = array();
+    public function tambahProduk($produk) {
+        $this->daftarProduk[] = $produk;
+    }
+    public function cetak() {
+        $str = "DAFTAR PRRODUK : <br>";
+        foreach ($this->daftarProduk as $produk) {
+            $str .= "- {$produk->getInfoProduk()} <br>";
+        }
+        return $str;
+    }
+}
---
+++ namespace/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("FFXIV", "Naoki Yoshida", "SQ",500000, 1000);
+// $produk2 = new Game("Final Fantasy Ultimania Archive Volume 1","Square Enix","Dark Horse Comics",800000 ,50);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo  $cetakProduk->cetak();
+
+// echo  "<hr>";
+
+use App\Service\User as  ServiceUser;
+use App\Produk\User as ProdukUser;
+new ServiceUser();
+echo '<br>';
+new ProdukUser();
